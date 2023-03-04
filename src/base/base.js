1770dc945a1a4ac332e580ed707cb232a0da7231
Lysak-Anna <hvlysak@gmail.com>
2023-03-04 21:46:49 +0300

    test-project

--- src/base/base.js
+++ src/base/base.js
@@ -1,6 +1,6 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: 'AIzaSyDxGhTAKJjz9dmokluOz0WgGiGaHAF4P68',
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
