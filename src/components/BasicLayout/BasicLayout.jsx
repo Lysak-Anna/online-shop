94ae08212341f84f02830799f7ba76e84ff48d33
Lysak-Anna <hvlysak@gmail.com>
2023-03-16 18:57:42 +0300

    delete console.log

--- src/components/BasicLayout/BasicLayout.jsx
+++ src/components/BasicLayout/BasicLayout.jsx
@@ -4,7 +4,6 @@ import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import { CommonContainer } from './BasicLayout.styled';
 export default function BasicLayout() {
-  console.log(process.env.REACT_APP_API_KEY);
   return (
     <CommonContainer>
       <Header />
